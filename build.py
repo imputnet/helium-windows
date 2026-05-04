e6364761130b169643a2eae5a314b28492765a00
wukko <me@wukko.me>
2026-05-04 15:56:14 +0600

    build: update script name

--- build.py
+++ build.py
@@ -9,7 +9,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """
-ungoogled-chromium build script for Microsoft Windows
+Helium build script for Windows
 """
 
 import sys
